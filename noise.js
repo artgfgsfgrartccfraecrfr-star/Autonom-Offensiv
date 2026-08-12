f52704c7a2d997fa448ea243ad8b2d95544cda21
artgfgsfgrartccfraecrfr-star <artgfgsfgrartccfraecrfr@gmail.com>
2026-08-13 01:13:51 +0200

    Create noise.js

---
+++ noise.js
@@ -0,0 +1,58 @@
+const canvas = document.getElementById("noise");
+const ctx = canvas.getContext("2d");
+
+let width;
+let height;
+
+function resize() {
+    width = Math.floor(window.innerWidth / 3);
+    height = Math.floor(window.innerHeight / 3);
+
+    canvas.width = width;
+    canvas.height = height;
+}
+
+function generateNoise() {
+
+    const imageData = ctx.createImageData(width, height);
+    const data = imageData.data;
+
+    for (let i = 0; i < data.length; i += 4) {
+
+        /*
+         * Mostly black.
+         * Occasionally dark gray.
+         */
+
+        const random = Math.random();
+
+        let value;
+
+        if (random > 0.985) {
+            value = Math.floor(Math.random() * 35) + 10;
+        } else if (random > 0.90) {
+            value = Math.floor(Math.random() * 18) + 5;
+        } else {
+            value = Math.floor(Math.random() * 7);
+        }
+
+        data[i] = value;
+        data[i + 1] = value;
+        data[i + 2] = value;
+        data[i + 3] = 255;
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+}
+
+function animate() {
+
+    generateNoise();
+
+    requestAnimationFrame(animate);
+}
+
+window.addEventListener("resize", resize);
+
+resize();
+animate();
